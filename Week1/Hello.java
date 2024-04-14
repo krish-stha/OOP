2e152c965a4ca1f48c64597cfa2cd1e156dfa481
krish <krishstha202@gmail.com>
2024-04-14 17:31:00 +0900

    next question

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,6 +2,20 @@ package Week1;
 
 public class Hello{
     public static void main(String[] args){
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello WOrld");
+
+        /*2. Extend/Modify the above program to print two additional lines:
+
+        This is my first program
+
+        I am on module ST4003CEM
+
+        System.out.print (not println) and produces the same output. */
+        
+        System.out.print("This is my first program\n");
+        System.out.print("I am on module ST4003CEM");
+
+
     }
 }
