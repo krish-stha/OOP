2059454ce583d3ef314e0fa67b92eaf9ff8c05cc
krish <krishstha202@gmail.com>
2024-05-05 12:15:59 +0900

    Update Task1.java

--- Week1/Task1.java
+++ Week1/Task1.java
@@ -56,7 +56,7 @@ public class Task1{
              System.out.println("**");
 
              System.out.println("***");
-             System.out.println("****");
+             System.out.println("****" ); 
 
              /*5. WAP prints the following pattern:
 
@@ -92,12 +92,12 @@ public class Task1{
 
 */
 
-        System.out.println("A well-formed Java program has a main method with { and } braces.");
-        System.out.println("\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
-        System.out.println("\n(But we type \\\" instead!)\n");
-        System.out.println("What is the difference between\na ' and a \"?  Or between a \" and a \\\"?\n");
-        System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
-  
+            System.out.println("A well-formed Java program has a main method with { and } braces.");
+            System.out.println("\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+            System.out.println("\n(But we type \\\" instead!)\n");
+            System.out.println("What is the difference between\na ' and a \"?  Or between a \" and a \\\"?\n");
+            System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+    
 
     }
 }
